b5db949aee92e4589cfa44042941c8731f4bb811
kaktuszjoe <petigecisanyi@gmail.com>
2023-05-18 14:31:01 +0200

    tesztek futnak

--- src/test/app.spec.js
+++ src/test/app.spec.js
@@ -1,4 +1,25 @@
-describe("szamitasteszt", () => {
-    it('A visszadott elem tartalma', () => {
-        let actual  = createTd().textContent == 'valami'
-        expect(actual).toBe(true)})})
+//JakabosÁbel 2023.05.18 , sz/i/n
+
+describe("szamitasteszt1", () => {
+    it('szamitas1', () => {
+        let actual  =  getUnsuccessPercent(100,100)
+        expect(actual).toEqual(100)})
+
+
+    it('szamitas2', () => {
+            let actual  =  getUnsuccessPercent(10,10)
+            expect(actual).toEqual(100)})
+
+
+            it('szamitas3', () => {
+                let actual  =  getUnsuccessPercent(10,0)
+                 expect(actual).toEqual(0)})
+
+
+         it('kivonas', () => {
+         let actual  =  getUnsuccessCount(45,12)
+             expect(actual).toEqual(33)})
+
+         });
+                       
+ 
